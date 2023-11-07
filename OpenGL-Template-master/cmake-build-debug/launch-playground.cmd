c75193ae6e531121fe1246bfd28a8c5f19ef125b
Malte Lanz <55878865+ML200@users.noreply.github.com>
2023-11-07 20:00:10 +0100

    Initial commit

---
+++ OpenGL-Template-master/cmake-build-debug/launch-playground.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/m/Downloads/OpenGL-Template-master/OpenGL-Template-master/playground/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\m\Downloads\OpenGL-Template-master\OpenGL-Template-master\cmake-build-debug\playground.exe"  
+pause
