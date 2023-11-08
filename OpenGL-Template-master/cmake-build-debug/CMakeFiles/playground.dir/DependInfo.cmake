824629890176eef302c813e89a68924e079d3e7e
Malte Lanz <55878865+ML200@users.noreply.github.com>
2023-11-08 17:03:45 +0100

    Ortho update

---
+++ OpenGL-Template-master/cmake-build-debug/CMakeFiles/playground.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/m/OneDrive/Dokumente/GitHub/2dProject/OpenGL-Template-master/common/shader.cpp" "CMakeFiles/playground.dir/common/shader.cpp.obj" "gcc" "CMakeFiles/playground.dir/common/shader.cpp.obj.d"
+  "C:/Users/m/OneDrive/Dokumente/GitHub/2dProject/OpenGL-Template-master/playground/playground.cpp" "CMakeFiles/playground.dir/playground/playground.cpp.obj" "gcc" "CMakeFiles/playground.dir/playground/playground.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/m/OneDrive/Dokumente/GitHub/2dProject/OpenGL-Template-master/cmake-build-debug/external/glfw-3.1.2/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "C:/Users/m/OneDrive/Dokumente/GitHub/2dProject/OpenGL-Template-master/cmake-build-debug/external/CMakeFiles/GLEW_1130.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpenGL-Template-master/cmake-build-debug/CMakeFiles/playground.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/playground.dir/common/shader.cpp.obj"
+  "CMakeFiles/playground.dir/common/shader.cpp.obj.d"
+  "CMakeFiles/playground.dir/playground/playground.cpp.obj"
+  "CMakeFiles/playground.dir/playground/playground.cpp.obj.d"
+  "libplayground.dll.a"
+  "playground.exe"
+  "playground.exe.manifest"
+  "playground.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/playground.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
